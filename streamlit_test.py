941c9246058d5e26c14a8dce7b1a88cb9b5eef36
Sophia Scherer <srschere@ncsu.edu>
2023-04-02 00:04:32 -0400

    Finished implementing interactive prototype visualizations for fake demo data

--- streamlit_test.py
+++ streamlit_test.py
@@ -29,12 +29,38 @@ chart_data = pd.DataFrame(
 st.line_chart(chart_data)
 
 
-st.write("We can also visualize percentage demographics with an interactive pie chart:")
+
 demographic_data = pd.DataFrame({
     'count': [20, 6, 15, 3],
     'ethnicity': ['White', 'Black', 'Asian', 'Hispanic']
 })
 
 st.subheader("Pie Chart")
+st.write("We can also visualize percentage demographics with an interactive pie chart:")
 pieChart = px.pie(demographic_data, values = 'count', names = 'ethnicity', title = 'Application Demographics 202X')
 st.plotly_chart(pieChart)
+
+
+st.subheader("Scatter Plot")
+st.write("A scatter plot can be used to show correlation between two metrics:")
+gpa_data = pd.DataFrame({
+    'gpa': [3.4, 2, 4.0, 2.4, 3.9, 3.2, 1.9],
+    'recommend': [3, 1, 4, 2, 4, 3, 0]
+})
+gpaCorrelation = px.scatter(gpa_data, x = 'gpa', y = 'recommend', title = 'GPA as a Predictor of Recommendation')
+gpaCorrelation.update_traces(marker_size=10)
+st.plotly_chart(gpaCorrelation)
+
+st.subheader("Bar Chart")
+st.write("A bar chart can be used to show numbers of items in different categories:")
+year_data = pd.DataFrame({
+    'applications received': [20,34, 40, 67],
+    'year': ['2019', '2020', '2021', '2022']
+})
+barChart = px.bar(year_data, x='year', y='applications received')
+st.plotly_chart(barChart)
+
+
+st.write("This data could also be displayed in a plotly line chart to show chronological progression:")
+timeChart = px.line(year_data, x='year', y='applications received', title = 'Number of Applications Received Over Time')
+st.plotly_chart(timeChart)
